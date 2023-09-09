1c01a7a5c443b13cf789013b1e83e524a758799c
fcd <1352288+femnad@users.noreply.github.com>
2023-09-10 00:13:12 +0100

    the usual LED confusion

--- keyboards/moonlander/keymaps/foolmap/fn.c
+++ keyboards/moonlander/keymaps/foolmap/fn.c
@@ -83,7 +83,7 @@ void keyboard_post_init_user(void) {
     rgb_matrix_enable_noeeprom();
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
     // Probably needed.
-    // rgb_matrix_sethsv_noeeprom(HSV_OFF);
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
     // Probably not needed?
     // rgb_matrix_set_color_all(0, 0, 0);
 }
