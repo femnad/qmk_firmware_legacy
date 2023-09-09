407b7ca8cbcc3a77468938b2c499826a500f9c00
fcd <1352288+femnad@users.noreply.github.com>
2023-09-10 00:28:44 +0100

    ergodox ez, moonlander and preonic keymaps
    add ergodox ez keymap
    add moonlander keymap
    add preonic keymap
    add foolmap tr layout
    make the type checker happy
    undo mistakes while appeasing the type god
    add one more long button
    update reset and page down keys
    ergodox ez: rename media layer, move boot button
    Also add kc_app key, for binding to screen lock
    use meh keys instead of depending on existing keycodes
    actually use CtrlAltGui (CAG) for hyper w/o shift
    Also apply the yearly formatting
    ergodox ez: reposition f11 and f12
    ergodox ez: add hyper and non-oneshot gui keys
    Useful for conformist WMs
    ergodox ez: make hyper one shot, use correct gui
    rewrite keymap writes, consolidate keymap writers
    and move them somewhere else
    preonic: migrate to foolmap
    update preonic readme
    or is it the drop revision?
    foolmapify foolmap-tr
    update readme
    preonic: replace gui and mdia
    preonic: don't mess up the modifiers
    preonic: (re-)add volume keys
    moonlander: add cag+l key
    preonic: shuffle boot and media keys
    moonlander: reset is called boot now
    moonlander: I have no idea how this works
        And I left my keyboard in the office.
    preonic: swap media key pairs
    preonic: move volume keys to mmedia layer
    ergodox-ez: don't use CAG keys
    remove one shot modifier from control
    ergodox_ez: remove tag toggle
    ergodox_ez: fiddle with one tap settings
    ergodox_ez: even shorter oneshot timeout
    preonic: fix readme
    preonic: further fix readme
    preonic foolmap-tr: add lower case chars
    preonic foolmap-tr: fix lowercase ı
    preonic foolmap-tr: move direction keys
    preonic: move symb keys to middle column, shuffle others
    Nope, causes increased hand movement
        So, moonlander layout is also wrong then?
        Revert "preonic: move symb keys to middle column, shuffle others"
        This reverts commit 5b64d5bf025f27e6462c7e5db4b76fe5b829f821.
    moonlander: move shift to thumb cluster, shuffle others
        Because someone said the moonlander layout is also wrong.
    the usual LED confusion
    moonlander: missing modifications to keymap file
    moonlander: nope didn't work
        pros:
        * Enter, tab, backspace, esc are easier to reach
        cons:
        * shift harder to reach
        * page and direction keys no longer under home row
        * harder to combine symb with ctrl and shift
    moonlander: led fail as usual
    moonlander: keymap.c update fail
    moonlander: add rgb management keys
    moonlander: fix rgb key names
    moonlander: crapshoot
    hide bugs rather than fixing them
    moonlander: move rgb toggle key
    moonlander: get too confident and remove comments
    ergodox_ez: hyper out gui in
    ergodox_ez: use empty keys
    ergodox_ez: initial swap hands attempt
    ergodox_ez: try swap hands after script fix
    ergodox_ez: swap_config in already defined in ergodox.c
        d'oh!
    ergodox_ez: fix keycode
    ergodox_ez: clear turns swap hands off
    ergodox_ez: swap hands is a toggle, increase oneshot tap toggle
    ergodox_ez: undefine tapping toggle first
    ergodox_ez: actually sh_os is the one I want
    ergodox_ez: only sh_tt can toggle it seems

---
+++ keyboards/preonic/keymaps/foolmap-tr/fn.c
@@ -0,0 +1,15 @@
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case CLEAR:
+        clear_oneshot_mods();
+        clear_oneshot_locked_mods();
+        clear_keyboard();
+        reset_oneshot_layer();
+        layer_clear();
+        layer_on(BASE);
+        return false;
+    }
+ }
+  return true;
+}
