dc5431bcc959b844a6f4874e4dc982c63d2665f5
fcd <1352288+femnad@users.noreply.github.com>
2023-09-10 01:04:03 +0100

    moonlander: nope didn't work
    pros:
    
    * Enter, tab, backspace, esc are easier to reach
    
    cons:
    
    * shift harder to reach
    * page and direction keys no longer under home row
    * harder to combine symb with ctrl and shift

--- keyboards/moonlander/keymaps/foolmap/keymap.c
+++ keyboards/moonlander/keymaps/foolmap/keymap.c
@@ -15,15 +15,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Layer: BASE
  * +------+------+------+------+------+------+------+------+------+------+------+------+------+------+
- * |@MDIA |  1   |  2   |  3   |  4   |  5   |^LGUI |^RGUI |  6   |  7   |  8   |  9   |  0   |@MDIA |
+ * |@MDIA |  1   |  2   |  3   |  4   |  5   | ESC  | BSPC |  6   |  7   |  8   |  9   |  0   |@MDIA |
  * +------+------+------+------+------+------+------+------+------+------+------+------+------+------+
- * | MUTE |  '   |  ,   |  .   |  P   |  Y   | ESC  | BSPC |  F   |  G   |  C   |  R   |  L   | VOLU |
+ * | MUTE |  '   |  ,   |  .   |  P   |  Y   | TAB  | ENT  |  F   |  G   |  C   |  R   |  L   | VOLU |
  * +------+------+------+------+------+------+------+------+------+------+------+------+------+------+
- * | MPLY |  A   |  O   |  E   |  U   |  I   | TAB  | ENT  |  D   |  H   |  T   |  N   |  S   | VOLD |
+ * | MPLY |  A   |  O   |  E   |  U   |  I   |^LSFT |^RSFT |  D   |  H   |  T   |  N   |  S   | VOLD |
  * +------+------+------+------+------+------+------+------+------+------+------+------+------+------+
  * |CLEAR |  ;   |  Q   |  J   |  K   |  X   |             |  B   |  M   |  W   |  V   |  Z   |CLEAR |
  * +------+------+------+------+------+------+             +------+------+------+------+------+------+
- * | HOME | PGUP | PGDN | END  |@SYMB |^LSFT |             |^LSFT |@SYMB | LEFT | DOWN |  UP  |RIGHT |
+ * |^LGUI | HOME | PGUP | PGDN | END  |@SYMB |             |@SYMB | LEFT | DOWN |  UP  |RIGHT |^RGUI |
  * +------+------+------+------+------+------+             +------+------+------+------+------+------+
  *                      | SPC  |^LCTL |^LALT |             |^RALT |^RCTL | SPC  |
  *                      +------+------+------+             +------+------+------+
@@ -31,12 +31,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = LAYOUT_moonlander(
-        OSL(MDIA), KC_1,          KC_2,          KC_3,          KC_4,          KC_5,          OSM(MOD_LGUI), OSM(MOD_RGUI), KC_6,    KC_7,    KC_8,  KC_9,     KC_0, OSL(MDIA),
-        KC_MUTE,   KC_QUOT,       KC_COMM,       KC_DOT,        KC_P,          KC_Y,          KC_ESC,        KC_BSPC,       KC_F,    KC_G,    KC_C,  KC_R,     KC_L, KC_VOLU,
-        KC_MPLY,   KC_A,          KC_O,          KC_E,          KC_U,          KC_I,          KC_TAB,        KC_ENT,        KC_D,    KC_H,    KC_T,  KC_N,     KC_S, KC_VOLD,
-        CLEAR,     KC_SCLN,       KC_Q,          KC_J,          KC_K,          KC_X,          KC_B,          KC_M,          KC_W,    KC_V,    KC_Z,  CLEAR,
-        KC_HOME,   KC_PGUP,       KC_PGDN,       KC_END,        OSL(SYMB),     OSM(MOD_LSFT), OSM(MOD_LSFT), OSL(SYMB),     KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
-        KC_SPC,    OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
+        OSL(MDIA),     KC_1,          KC_2,          KC_3,          KC_4,          KC_5,      KC_ESC,        KC_BSPC,       KC_6,    KC_7,  KC_8,     KC_9,          KC_0, OSL(MDIA),
+        KC_MUTE,       KC_QUOT,       KC_COMM,       KC_DOT,        KC_P,          KC_Y,      KC_TAB,        KC_ENT,        KC_F,    KC_G,  KC_C,     KC_R,          KC_L, KC_VOLU,
+        KC_MPLY,       KC_A,          KC_O,          KC_E,          KC_U,          KC_I,      OSM(MOD_LSFT), OSM(MOD_RSFT), KC_D,    KC_H,  KC_T,     KC_N,          KC_S, KC_VOLD,
+        CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          KC_X,      KC_B,          KC_M,          KC_W,    KC_V,  KC_Z,     CLEAR,
+        OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        OSL(SYMB), OSL(SYMB),     KC_LEFT,       KC_DOWN, KC_UP, KC_RIGHT, OSM(MOD_RGUI),
+        KC_SPC,        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
 ),
 
 /* Layer: SYMB
