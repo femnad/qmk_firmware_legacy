e8c7d69650de7acb6dcd0f19047098ee6990d442
fcd <1352288+femnad@users.noreply.github.com>
2024-01-08 01:17:18 +0300

    ergodox_ez: initial swap hands attempt

--- keyboards/ergodox_ez/keymaps/foolmap/keymap.c
+++ keyboards/ergodox_ez/keymaps/foolmap/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |@SYMB | LGUI |       | RGUI |@SYMB |
  *                                 ,------|------|------|       |------+------+------.
- *                                 | SPC  |^LCTL |      |       |      |^RCTL | SPC  |
+ *                                 | SPC  |^LCTL |SH_TOGG|       |SH_TOGG|^RCTL | SPC  |
  *                                 |      |      |------|       |------|      |      |
  *                                 |      |      |^LALT |       |^RALT |      |      |
  *                                 `--------------------'       `--------------------'
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          KC_X,    OSM(MOD_LSFT), OSM(MOD_RSFT), KC_B,     KC_M,          KC_W, KC_V,    KC_Z, CLEAR,
         OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT, OSM(MOD_RGUI),
         OSL(SYMB),     KC_LGUI,       KC_RGUI,       OSL(SYMB),
-        _______,       _______,
+        KC_SH_TOGG,    KC_SH_TOGG,
         KC_SPC,        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
 ),
 
@@ -106,6 +106,111 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+void clear(void) {
+  clear_oneshot_mods();
+  clear_oneshot_locked_mods();
+  clear_keyboard();
+  reset_oneshot_layer();
+  layer_clear();
+  layer_on(BASE);
+  rgb_matrix_sethsv_noeeprom(HSV_OFF);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case CLEAR:
+        clear();
+        return false;
+    }
+  }
+  return true;
+}
+
+// Runs whenever there is a layer state change.
+layer_state_t layer_state_set_user(layer_state_t state) {
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+
+  uint8_t layer = get_highest_layer(state);
+  switch (layer) {
+      case 0:
+        break;
+      case 1:
+        ergodox_right_led_1_on();
+        break;
+      case 2:
+        ergodox_right_led_2_on();
+        break;
+      case 3:
+        ergodox_right_led_3_on();
+        break;
+      case 4:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        break;
+      case 5:
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+        break;
+      case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        break;
+      case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        break;
+      default:
+        break;
+    }
+
+  return state;
+};
+
+void oneshot_mods_changed_user(uint8_t mods) {
+  if (mods & MOD_MASK_SHIFT) {
+    rgb_matrix_sethsv_noeeprom(HSV_GREEN);
+  }
+  if (mods & MOD_MASK_CTRL) {
+    rgb_matrix_sethsv_noeeprom(HSV_BLUE);
+  }
+  if (mods & MOD_MASK_ALT) {
+    rgb_matrix_sethsv_noeeprom(HSV_PURPLE);
+  }
+  if (mods & MOD_MASK_GUI) {
+    rgb_matrix_sethsv_noeeprom(HSV_CORAL);
+  }
+  if (!mods) {
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+  }
+}
+
+void oneshot_locked_mods_changed_user(uint8_t mods) {
+  if (mods & MOD_MASK_SHIFT) {
+    rgb_matrix_sethsv_noeeprom(HSV_SPRINGGREEN);
+  }
+  if (mods & MOD_MASK_CTRL) {
+    rgb_matrix_sethsv_noeeprom(HSV_CYAN);
+  }
+  if (mods & MOD_MASK_ALT) {
+    rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
+  }
+  if (mods & MOD_MASK_GUI) {
+    rgb_matrix_sethsv_noeeprom(HSV_PINK);
+  }
+  if (!mods) {
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+  }
+}
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(HSV_OFF);
+}
 void clear(void) {
   clear_oneshot_mods();
   clear_oneshot_locked_mods();
