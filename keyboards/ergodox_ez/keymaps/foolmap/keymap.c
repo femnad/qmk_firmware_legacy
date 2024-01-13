9178ad85826b525e5d24a3c6a544d304be915e4b
fcd <1352288+femnad@users.noreply.github.com>
2024-01-13 15:50:07 +0300

    ergodox_ez: sh_os replaces {l,r}gui

--- keyboards/ergodox_ez/keymaps/foolmap/keymap.c
+++ keyboards/ergodox_ez/keymaps/foolmap/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |^LGUI | HOME | PGUP | PGDN | END  |                                       | LEFT | DOWN |  UP  |RIGHT |^RGUI |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |@SYMB | LGUI |       | RGUI |@SYMB |
+ *                                        |@SYMB |SH_OS |       |SH_OS |@SYMB |
  *                                 ,------|------|------|       |------+------+------.
  *                                 | SPC  |^LCTL |SH_TT |       |SH_TT |^RCTL | SPC  |
  *                                 |      |      |------|       |------|      |      |
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPLY,       KC_A,          KC_O,          KC_E,          KC_U,          KC_I,    KC_D,          KC_H,          KC_T,     KC_N,          KC_S, KC_VOLD,
         CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          KC_X,    OSM(MOD_LSFT), OSM(MOD_RSFT), KC_B,     KC_M,          KC_W, KC_V,    KC_Z, CLEAR,
         OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT, OSM(MOD_RGUI),
-        OSL(SYMB),     KC_LGUI,       KC_RGUI,       OSL(SYMB),
+        OSL(SYMB),     SH_OS,         SH_OS,         OSL(SYMB),
         SH_TT,         SH_TT,
         KC_SPC,        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
 ),
