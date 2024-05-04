f369de65601d6932b96137773858dbb69a673085
fcd <1352288+femnad@users.noreply.github.com>
2024-05-04 17:17:45 +0300

    ergodox_ez/foolmap-tr: too sensitive for one shot mod

--- keyboards/ergodox_ez/keymaps/foolmap-tr/keymap.c
+++ keyboards/ergodox_ez/keymaps/foolmap-tr/keymap.c
@@ -64,20 +64,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |@SYMB | LGUI |       | RGUI |@SYMB |
  *                                 ,------|------|------|       |------+------+------.
- *                                 | SPC  |^LCTL |SH_OS |       |SH_OS |^RCTL | SPC  |
+ *                                 | SPC  | LCTL |SH_OS |       |SH_OS | RCTL | SPC  |
  *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |^LALT |       |^RALT |      |      |
+ *                                 |      |      | LALT |       | RALT |      |      |
  *                                 `--------------------'       `--------------------'
  */
 [BASE] = LAYOUT_ergodox_pretty(
-        OSL(MISC),     KC_1,          KC_2,          KC_3,          KC_4,          KC_5,    KC_ESC,        KC_BSPC,       KC_6,     KC_7,          KC_8, KC_9,      KC_0, OSL(MISC),
-        KC_MUTE,       KC_QUOT,       KC_COMM,       KC_DOT,        KC_P,          KC_Y,    KC_TAB,        KC_ENT,        KC_F,     KC_G,          KC_C, KC_R,      KC_L, KC_VOLU,
-        OSL(TRUM),     KC_A,          KC_O,          KC_E,          KC_U,          KC_I,    KC_D,          KC_H,          KC_T,     KC_N,          KC_S, OSL(TRUM),
-        CLEAR,         KC_SCLN,       KC_Q,          KC_J,          KC_K,          KC_X,    OSM(MOD_LSFT), OSM(MOD_RSFT), KC_B,     KC_M,          KC_W, KC_V,      KC_Z, CLEAR,
-        OSM(MOD_LGUI), KC_HOME,       KC_PGUP,       KC_PGDN,       KC_END,        KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT, OSM(MOD_RGUI),
-        OSL(SYMB),     KC_LGUI,       KC_RGUI,       OSL(SYMB),
+        OSL(MISC),     KC_1,    KC_2,    KC_3,      KC_4,    KC_5,    KC_ESC,        KC_BSPC,       KC_6,     KC_7,          KC_8, KC_9,      KC_0, OSL(MISC),
+        KC_MUTE,       KC_QUOT, KC_COMM, KC_DOT,    KC_P,    KC_Y,    KC_TAB,        KC_ENT,        KC_F,     KC_G,          KC_C, KC_R,      KC_L, KC_VOLU,
+        OSL(TRUM),     KC_A,    KC_O,    KC_E,      KC_U,    KC_I,    KC_D,          KC_H,          KC_T,     KC_N,          KC_S, OSL(TRUM),
+        CLEAR,         KC_SCLN, KC_Q,    KC_J,      KC_K,    KC_X,    OSM(MOD_LSFT), OSM(MOD_RSFT), KC_B,     KC_M,          KC_W, KC_V,      KC_Z, CLEAR,
+        OSM(MOD_LGUI), KC_HOME, KC_PGUP, KC_PGDN,   KC_END,  KC_LEFT, KC_DOWN,       KC_UP,         KC_RIGHT, OSM(MOD_RGUI),
+        OSL(SYMB),     KC_LGUI, KC_RGUI, OSL(SYMB),
         SH_OS,         SH_OS,
-        KC_SPC,        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_RALT), OSM(MOD_RCTL), KC_SPC
+        KC_SPC,        KC_LCTL, KC_LALT, KC_RALT,   KC_RCTL, KC_SPC
 ),
 
 /* Keymap 1: symb
